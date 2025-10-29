ec06e6cea28de2dbae95745a46eba325843512cc
kamran saberifard <kamisaberi@gmail.com>
2025-10-29 18:40:29 +0330

    code started but not finished yet

---
+++ station/src/main.cpp
@@ -0,0 +1,28 @@
+#include <QInternal>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "backend/app/StationApp.h"
+#include "backend/comms/CoreClient.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    // Register the CoreClient type so QML can instantiate it
+    qmlRegisterType<aegis::station::comms::CoreClient>("Aegis.Backend", 1, 0, "CoreClient");
+
+    QQmlApplicationEngine engine;
+    
+    // Load Main QML
+    const QUrl url(u"qrc:/Aegis/Station/src/frontend/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    
+    engine.load(url);
+
+    return app.exec();
+}
